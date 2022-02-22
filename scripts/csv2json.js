7c21beebc56d6898462490b1d5ff37dea4a1f9c1
Igor Spasić <igor.spasic@gmail.com>
2022-02-22 05:44:16 +0100

    Dopuna skripte za konverziju u JSON

--- scripts/csv2json.js
+++ scripts/csv2json.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const fs = require("fs");
 
 const json = {}
@@ -5,8 +7,9 @@ const json = {}
 const data = fs.readFileSync("imena.csv").toString();
 
 const rows = data.split("\n").slice(1);
+
 rows.forEach((row) => {
-    columns = row.split(",");
+    const columns = row.split(",");
     json[columns[0]] = columns[1]
 })
 
