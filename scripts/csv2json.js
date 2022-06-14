203785207022b4e8a265e2a56f8cad993dbf0a95
Filip Ilic <ilic.filip@gmail.com>
2022-06-14 16:32:41 +0200

    sort alphabetically

--- scripts/csv2json.js
+++ scripts/csv2json.js
@@ -10,7 +10,7 @@ const data = fs.readFileSync( '../imena.csv' ).toString();
 const rows = data.split( "\n" ).slice( 1 );
 
 rows.forEach( ( row ) => {
-    const columns = row.split( "," );
+    const columns = row.split( ',' );
     json[columns[0]] = columns[1]
 
     array.push( {
