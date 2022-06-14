f32a892b6413ac2596599284bf0e1e1dba42f503
Filip Ilic <ilic.filip@gmail.com>
2022-06-14 18:29:52 +0200

    fetch CSV content from google docs

---
+++ scripts/fetch-sheet.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const fs = require( 'fs' );
+const {GoogleSpreadsheet} = require( 'google-spreadsheet' );
+
+( async () => {
+    const creds = require( '../config/vokativi-7bb7924e5b47.json' );
+    const doc = new GoogleSpreadsheet( '1DOw_-l-ndcFfXWhD4fcpBx9f0dt9HypYIzLaAkQis_Q' );
+    await doc.useServiceAccountAuth( creds );
+
+    await doc.loadInfo(); // loads document properties and worksheets
+
+    const sheet = doc.sheetsByTitle['vokativi']
+    const rows = await sheet.getRows();
+
+    let csv = 'IME,VOKATIV' + "\n";
+
+    rows.forEach( ( row ) => {
+        csv += row._rawData[0] + ',' + row._rawData[1] + "\n"
+    } )
+
+    fs.writeFileSync( '../imena.csv', csv.trim() );
+} )();
