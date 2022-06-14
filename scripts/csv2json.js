7c94fca284c4c92dcbd76d8cec856f3c994782ce
Filip Ilic <ilic.filip@gmail.com>
2022-06-14 15:56:58 +0200

    script update

--- scripts/csv2json.js
+++ scripts/csv2json.js
@@ -1,18 +1,25 @@
 'use strict';
 
-const fs = require("fs");
+const fs = require( 'fs' );
 
 const json = {}
+let array = [];
 
-const data = fs.readFileSync("imena.csv").toString();
+const data = fs.readFileSync( '../imena.csv' ).toString();
 
-const rows = data.split("\n").slice(1);
+const rows = data.split( "\n" ).slice( 1 );
 
-rows.forEach((row) => {
-    const columns = row.split(",");
+rows.forEach( ( row ) => {
+    const columns = row.split( "," );
     json[columns[0]] = columns[1]
-})
 
-fs.writeFileSync("imena.json", JSON.stringify(json));
+    array.push( {
+        'IMENA': columns[0],
+        'VOKATIV': columns[1],
+    } );
+} )
 
-console.log("Done.")
+fs.writeFileSync( '../imena.json', JSON.stringify( json ) );
+fs.writeFileSync( '../vokativi.json', JSON.stringify( array, null, 2 ) );
+
+console.log( 'Done.' )
